f4fcb6f40aaf5137531652bd7a984841f4e622aa
trongminhle <trongminhjrfo3@gmail.com>
2021-12-29 23:54:40 +0300

    update 29/12 - 03

--- Lab 2/src/test.c
+++ Lab 2/src/test.c
@@ -17,9 +17,9 @@ bool make_init_heap() {
 struct test_result test_1() {
     printf(" test 1: normal successful memory allocation \n");
     debug_heap(stdout, block);
-    void *mem = _malloc(1000);
+    void *q = _malloc(500);
     debug_heap(stdout, block);
-    _free(mem);
+    _free(q);
     printf("========================================\n\n");
     return out_res;
 }
@@ -27,12 +27,12 @@ struct test_result test_1() {
 
 struct test_result test_2() {
     printf(" test 2: freeing one block from several allocated \n");
-    void *mem_1 = _malloc(1234);
-    void *mem_2 = _malloc(5678);
+    void *block_1 = _malloc(1234);
+    void *block_2 = _malloc(5678);
     debug_heap(stdout, block);
-    _free(mem_1);
+    _free(block_1);
     debug_heap(stdout, block);
-    _free(mem_2);
+    _free(block_2);
     printf("========================================\n\n");
     return out_res;
 }
@@ -40,14 +40,14 @@ struct test_result test_2() {
 
 struct test_result test_3() {
     printf(" test 3: release of the two blocks of several dedicated \n");
-    void* mem_1 = _malloc(2000);
-    void* mem_2 = _malloc(3000);
-    void* mem_3 = _malloc(4000);
+    void* block_1 = _malloc(2000);
+    void* block_2 = _malloc(3000);
+    void* block_3 = _malloc(4000);
     debug_heap(stdout, block);
-    _free(mem_1);
-    _free(mem_2);
+    _free(block_1);
+    _free(block_2);
     debug_heap(stdout, block);
-    _free(mem_3);
+    _free(block_3);
     printf("========================================\n\n");
     return out_res;
 }
@@ -55,11 +55,11 @@ struct test_result test_3() {
 struct test_result test_4() {
     printf(" test 4: memory has run out, the new memory region expands the old one \n");
     debug_heap(stdout, block);
-    void *mem_1 = _malloc(1234);
-    void *mem_2 = _malloc(2345);
+    void *block_1 = _malloc(1234);
+    void *block_2 = _malloc(2345);
     debug_heap(stdout, block);
-    _free(mem_1);
-    _free(mem_2);
+    _free(block_1);
+    _free(block_2);
     printf("========================================\n\n");
     return out_res;
 }
@@ -87,9 +87,9 @@ struct test_result test_5() {
     mmap_aloc(3210, addr);
 
     // test cannot extend allocate
-    void *allocated = _malloc(98765);
+    void *block_5 = _malloc(98765);
     debug_heap(stdout, block);
-    _free(allocated);
+    _free(block_5);
     printf("========================================\n\n");
     return out_res;
 }
