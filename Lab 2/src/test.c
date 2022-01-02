880f0bb2ddf6e5bf7747472720075a6ca4779899
trongminhle <trongminhjrfo3@gmail.com>
2022-01-02 23:14:33 +0300

    update 02/01/21 - 03

--- Lab 2/src/test.c
+++ Lab 2/src/test.c
@@ -28,9 +28,9 @@ struct test_result test_1() {
     debug_heap(stdout, block);
     void *q = _malloc(500);
     debug_heap(stdout, block);
-    //_free(q);
-
-    block_all_ed = q;
+    _free(q);
+    debug_heap(stderr, block);
+    //block_all_ed = q;
     printf("========================================\n\n");
     return out_res;
 }
@@ -38,12 +38,14 @@ struct test_result test_1() {
 
 struct test_result test_2() {
     printf(" test 2: freeing one block from several allocated \n");
+    debug_heap(stdout, block);
     void *block_1 = _malloc(1234);
-    //void *block_2 = _malloc(5678);
+    void *block_2 = _malloc(5678);
     debug_heap(stdout, block);
     _free(block_1);
-    debug_heap(stdout, block);
-    //_free(block_2);
+    //debug_heap(stdout, block);
+    _free(block_2);
+    debug_heap(stderr, block);
     printf("========================================\n\n");
     return out_res;
 }
@@ -67,6 +69,7 @@ struct test_result test_3() {
 
 struct test_result test_4() {
     printf(" test 4: memory has run out, the new memory region expands the old one \n");
+
     const size_t mem_size_init = 25000;
     debug_heap(stdout, block);
 
@@ -85,30 +88,10 @@ struct test_result test_4() {
 
     debug_heap(stderr, heap);
 
-    printf("========================================\n\n");
-    return out_res;
-}
-
-struct test_result test_5() {
-    printf(" test 5: memory has run out, the old memory region cannot be expanded due to a different allocated range of addresses, the new region is allocated in a different place \n");
     /*
+    const size_t mem_size_init = 25000;
     debug_heap(stdout, block);
 
-    while (block->next != NULL)
-        block = block->next;
-
-    //void *addr = block + block->capacity.bytes;
-
-    //mmap_aloc(3210, addr);
-
-
-    map_pages_for_main((uint8_t*)heap+4*getpagesize(),3210,MAP_PRIVATE | MAP_ANONYMOUS );
-
-    void *block_5 = _malloc(98765);
-    debug_heap(stdout, block);
-    _free(block_5);
-     */
-    const size_t mem_size_init = 60000;
     struct block_header* temp = block;
     struct block_header* last;
     while(temp) {
@@ -117,34 +100,83 @@ struct test_result test_5() {
     }
     void* addr = (uint8_t*) last + size_from_capacity(last->capacity).bytes;
 
-    /*
-    addr = map_pages_for_main((uint8_t*) (getpagesize() * ((size_t) addr / getpagesize() +
-                                                           (((size_t) addr % getpagesize()) > 0))), 1000,
-                              MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE);
-    */
 
     map_pages_for_main((uint8_t*) (getpagesize() * ((size_t) addr / getpagesize() +
-                                        (((size_t) addr % getpagesize()) > 0))), 1000,
-                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE);
-
-    //printf("\nThe address at which the memory became unavailable: %p.\n", addr);
+                                                    (((size_t) addr % getpagesize()) > 0))), 1000, MAP_STACK);
     void* mem_block = _malloc(mem_size_init);
-    struct block_header* new_block = block_get_header(mem_block);
-
-
-    if(new_block->capacity.bytes != mem_size_init)
-        err("Memory allocation error: the size of the allocated memory does not match the requested one.");
-
-    if(new_block->is_free)
-        err("Memory allocation error: the block is not marked as occupied.");
+    //struct block_header* new_block = block_get_header(mem_block);
 
     debug_heap(stderr, heap);
     _free(mem_block);
 
-    if(!new_block->is_free)
-        err("An error occurred while clearing memory: the block was not marked as free.");
-
     debug_heap(stderr, heap);
+
+    */
+
+    printf("========================================\n\n");
+    return out_res;
+}
+
+static inline void mmap_region(size_t length, void *addr) {
+    size_t count = (size_t) addr / getpagesize();
+    size_t remains = (size_t) addr % getpagesize();
+
+    uint8_t *total = (uint8_t *) (getpagesize() * (count + (remains > 1)));
+
+    mmap(total, length, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+}
+
+#define TEST_GROW_HEAP_NOT_EXTENDING_LENGTH 1000
+#define TEST_GROW_HEAP_NOT_EXTENDING_ALLOCATE 10000
+
+
+struct test_result test_5() {
+    printf(" test 5: memory has run out, the new region is allocated in a different place \n");
+    debug_heap(stdout, block);
+/*
+    const size_t mem_size_init = 50000;
+    struct block_header* temp = block;
+    struct block_header* last;
+    while(temp) {
+        last = temp;
+        temp = temp->next;
+    }
+    void* addr = (uint8_t*) last + size_from_capacity(last->capacity).bytes;
+
+
+    addr = (uint8_t*) (getpagesize() * ((size_t) addr / getpagesize() + (((size_t) addr % getpagesize()) > 1)));
+
+
+    mmap(addr, 100000, PROT_READ | PROT_WRITE, MAP_FIXED_NOREPLACE | MAP_FIXED, 0, 0);
+
+    void* mem_block = _malloc(mem_size_init);
+    debug_heap(stderr, heap);
+
+
+    struct block_header* new_block = block_get_header(mem_block);
+    if(new_block->capacity.bytes != mem_size_init)
+        err("Error: the size of the allocated memory does not match the requested one.");
+    if(new_block->is_free)
+        err("Memory allocation error: the block is not marked as occupied.");
+
+    _free(mem_block);
+    debug_heap(stderr, heap);
+*/
+
+    while (block->next != NULL)
+        block = block->next;
+
+    void *addr = block + block->capacity.bytes;
+    mmap_region(TEST_GROW_HEAP_NOT_EXTENDING_LENGTH, addr);
+
+    void *allocated = _malloc(TEST_GROW_HEAP_NOT_EXTENDING_ALLOCATE);
+
+    if ((uint8_t *) block->next == (uint8_t *) allocated - offsetof(struct block_header, contents)) {
+        printf("ew region allocated in the old region\n");
+    }
+    debug_heap(stdout, block);
+
+    _free(allocated);
 
     return out_res;
 }
